5f9f793e839ff2c74bc5e719672303bff7beb9f6
Charlie Welch <welch.charlie16@gmail.com>
2022-02-17 15:23:46 -0600

    Adding slider to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,8 +4,12 @@ import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../firebase.config';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 import Spinner from '../components/Spinner';
 import shareIcon from '../assets/svg/shareIcon.svg';
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Listing() {
 	const [listing, setListing] = useState(null);
@@ -37,7 +41,19 @@ function Listing() {
 
 	return (
 		<main>
-			{/* SLIDER */}
+			<Swiper slidesPerView={1} pagination={{ clickable: true }}>
+				{listing.imgUrls.map((url, index) => (
+					<SwiperSlide key={index}>
+						<div
+							className="swiperSlideDiv"
+							style={{
+								background: `url(${listing.imgUrls[index]}) center no-repeat`,
+								backgroundSize: 'cover',
+							}}
+						></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
 
 			<div
 				className="shareIconDiv"
