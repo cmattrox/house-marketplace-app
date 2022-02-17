9b4bc7443c10f845a1a74fb4e27ab4202233c4e8
Charlie Welch <welch.charlie16@gmail.com>
2022-02-17 14:41:52 -0600

    Setting up the contact page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
 	return (
@@ -38,6 +39,7 @@ function App() {
 						path="/category/:categoryName/:listingId"
 						element={<Listing />}
 					/>
+					<Route path="/contact/:landlordId" element={<Contact />} />
 				</Routes>
 				<Navbar />
 			</Router>
