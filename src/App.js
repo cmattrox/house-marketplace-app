7ab56ce090ed6206ff6b7989def6c8f55ad68b54
Charlie Welch <welch.charlie16@gmail.com>
2022-02-17 14:11:26 -0600

    Begin building out listing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 function App() {
 	return (
@@ -33,6 +34,10 @@ function App() {
 						element={<ForgotPassword />}
 					/>
 					<Route path="/create-listing" element={<CreateListing />} />
+					<Route
+						path="/category/:categoryName/:listingId"
+						element={<Listing />}
+					/>
 				</Routes>
 				<Navbar />
 			</Router>
